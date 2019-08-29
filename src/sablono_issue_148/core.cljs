2aa9d9c20d9e2d68139a0f93d06cb169a74ea4f1
Pedro Girardi <pedro.rogerio.girardi@gmail.com>
2019-08-29 23:05:04 +0700

    Controlled Component 2

--- src/sablono_issue_148/core.cljs
+++ src/sablono_issue_148/core.cljs
@@ -2,18 +2,29 @@
   (:require
    [rum.core :as rum]))
 
+(defonce state-ref (atom ""))
+
 (rum/defc ControlledInput []
   [:input {:type "text"
            :value "Sablono"
            :on-change #(js/console.log (-> % .-target .-value))}])
 
+(rum/defc ControlledInput2 < rum/reactive []
+  [:input {:type "text"
+           :value (rum/react state-ref)
+           :on-change #(reset! state-ref (-> % .-target .-value))}])
+
 (rum/defc UncontrolledInput []
   [:input {:type "text"}])
 
-(rum/defc Root []
+(rum/defc Root < rum/reactive []
   [:div.w-30.flex.flex-column.pa3
    [:span.f5 "Controlled"]
    (ControlledInput)
+
+   [:span.f5.mt3 "Controlled with external state"]
+   (ControlledInput2)
+   [:span.f7 (rum/react state-ref)]
 
    [:span.f5.mt3 "Uncontrolled"]
    (UncontrolledInput)])
